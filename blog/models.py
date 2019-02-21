3ef2b19617ba81864b1e2889a8490c41687ee923
tbaror <tbaror@gmail.com>
2019-02-21 18:37:12 +0200

    jobs home creation

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+
+    title = models.CharField(max_length=300)
+    pub_date = models.DateTimeField()
+    body =  models.TextField(max_length=600)
+    image = models.ImageField(upload_to='images/')
